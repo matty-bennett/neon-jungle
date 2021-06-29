32e6f4cc83db36d5230dc15a34e23d43ab86175e
Savannah Bennett <savvy.bennett8@gmail.com>
2021-06-29 12:46:46 -0600

    jk - the JWT authentication is not working

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const { ApolloServer } = require('apollo-server-express');
 // const path = require('path');
 
 const { typeDefs, resolvers } = require('./schemas');
+const { authMiddleware } = require('./utils/auth');
 const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
@@ -11,7 +12,7 @@ const server = new ApolloServer({
     typeDefs,
     resolvers,
     playground: true,
-    // context: authMiddleware
+    context: authMiddleware
 });
 
 //integrate Apollo server w/ Express application as middleware
