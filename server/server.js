d18a08c9788b26ec1430deb39dcdec1bc563c39e
Savannah Bennett <savvy.bennett8@gmail.com>
2021-06-28 20:10:43 -0600

    played around w/file to get server to work. seems that 'app.once' is causing a problem but unsure why

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
-const path = require('path');
+// const path = require('path');
 
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
@@ -10,10 +10,12 @@ const app = express();
 const server = new ApolloServer({
     typeDefs,
     resolvers,
+    playground: true,
     // context: authMiddleware
 });
 
-// server.applyMiddleware({app});
+//integrate Apollo server w/ Express application as middleware
+server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
@@ -28,9 +30,9 @@ app.use(express.json());
 //     res.sendFile(path.join(__dirname, '../client/build/index.html'));
 // });
 
-app.once('open', () => {
-    app.listen(PORT, () => {
-        console.log(`API server running on port ${PORT}!`);
-        console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}!`);
-    });
+// app.once('open', () => {
+app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}!`);
 });
+// });
