667d0856e83ade1afae49d05adb3365010c20155
Savannah Bennett <savvy.bennett8@gmail.com>
2021-06-29 18:52:39 -0600

    authMiddleware JWT tokens working!

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -4,34 +4,34 @@ const secret = 'mysecretshhhhh';
 const expiration = '2h';
 
 module.exports = {
-    // authMiddleware: function ({ req }) {
-    //     let token = req.body.token || req.query.token || req.headers.authorization;
+    authMiddleware: function ({ req }) {
+        let token = req.body.token || req.query.token || req.headers.authorization;
 
-    //     //["Bearer", "<tokenvalue>"]
-    //     if (req.headers.authorization) {
-    //         token = token
-    //             .split(' ')
-    //             .pop()
-    //             .trim();
-    //     }
+        //["Bearer", "<tokenvalue>"]
+        if (req.headers.authorization) {
+            token = token
+                .split(' ')
+                .pop()
+                .trim();
+        }
 
-    //     console.log("token", token);
+        console.log("token", token);
 
-    //     if (!token) {
-    //         return req;
-    //     }
+        if (!token) {
+            return req;
+        }
 
-    //     try {
-    //         const { data } = jwt.verify(token, secret, { maxAge: expiration });
-    //         req.user = data;
-    //     }
+        try {
+            const { data } = jwt.verify(token, secret, { maxAge: expiration });
+            req.user = data;
+        }
 
-    //     catch {
-    //         console.log('Invalid token.');
-    //     }
+        catch {
+            console.log('Invalid token.');
+        }
 
-    //     return req;
-    // },
+        return req;
+    },
     signToken: function ({ firstName, email, _id }) {
         const payload = { firstName, email, _id };
 
