c24452fe36e2b8f5056d6529cc0002db728b6ba1
savbennett8 <savvy.bennett8@gmail.com>
2021-06-29 19:41:42 -0600

    Revert "changes from savvy branch"

--- server/models/index.js
+++
@@ -1,6 +0,0 @@
-const User = require('./User');
-const Category = require('./Category');
-const Product = require('./Product');
-const Order = require('./Order');
-
-module.exports = { Category, Product, User, Order };
