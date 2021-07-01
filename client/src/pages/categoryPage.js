2bb83e1e9de5d7fbb67f28de65a532c8ecabbb6e
Savannah Bennett <savvy.bennett8@gmail.com>
2021-07-01 17:01:54 -0600

    category page setup and rendering

---
+++ client/src/pages/categoryPage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Category from '../components/Category';
+
+function categoryPage() {
+    return (
+        <Category />
+    )
+}
+
+export default categoryPage;
