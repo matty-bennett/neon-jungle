7ac78300d1985f7346ccb2916cf5fd5ebf3286b2
Savannah Bennett <savvy.bennett8@gmail.com>
2021-07-04 16:14:17 -0600

    save from merge

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { loadStripe } from '@stripe/stripe-js';
-import { useLazyQuery } from '@apollo/client';
+// import { useLazyQuery } from '@apollo/client';
 
 import CartItem from '../CartItem';
 import Auth from '../../utils/auth';
@@ -9,13 +9,13 @@ import { useStoreContext } from "../../utils/GlobalState";
 import { TOGGLE_CART, ADD_MULTIPLE_TO_CART } from "../../utils/actions";
 import { idbPromise } from "../../utils/helpers";
 import { FaShoppingCart } from 'react-icons/fa';
-import { QUERY_CHECKOUT } from '../../utils/queries';
+// import { QUERY_CHECKOUT } from '../../utils/queries';
 
 const stripePromise = loadStripe('pk_test_TYooMQauvdEDq54NiTphI7jx');
 
 const Cart = () => {
     const [state, dispatch] = useStoreContext();
-    const [getCheckout, { data }] = useLazyQuery(QUERY_CHECKOUT);
+    // const [getCheckout, { data }] = useLazyQuery(QUERY_CHECKOUT);
 
     useEffect(() => {
         async function getCart() {
@@ -59,9 +59,9 @@ const Cart = () => {
             }
         });
 
-        getCheckout({
-            variables: { products: productIds }
-        });
+        // getCheckout({
+        //     variables: { products: productIds }
+        // });
 
         console.log(productIds)
     }
