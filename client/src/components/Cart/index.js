8868581887e1fac6a8c3568fcc394ba743711321
Savannah Bennett <savvy.bennett8@gmail.com>
2021-07-04 16:12:09 -0600

    tried to implement Stripe payment but it's not working and not sure how to fix

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -1,4 +1,7 @@
 import React, { useEffect } from 'react';
+import { loadStripe } from '@stripe/stripe-js';
+import { useLazyQuery } from '@apollo/client';
+
 import CartItem from '../CartItem';
 import Auth from '../../utils/auth';
 import './style.css';
@@ -6,9 +9,13 @@ import { useStoreContext } from "../../utils/GlobalState";
 import { TOGGLE_CART, ADD_MULTIPLE_TO_CART } from "../../utils/actions";
 import { idbPromise } from "../../utils/helpers";
 import { FaShoppingCart } from 'react-icons/fa';
+import { QUERY_CHECKOUT } from '../../utils/queries';
+
+const stripePromise = loadStripe('pk_test_TYooMQauvdEDq54NiTphI7jx');
 
 const Cart = () => {
     const [state, dispatch] = useStoreContext();
+    const [getCheckout, { data }] = useLazyQuery(QUERY_CHECKOUT);
 
     useEffect(() => {
         async function getCart() {
@@ -43,6 +50,22 @@ const Cart = () => {
         );
     }
 
+    function submitCheckout() {
+        const productIds = [];
+
+        state.cart.forEach((item) => {
+            for (let i = 0; i < item.purchaseQuantity; i++) {
+                productIds.push(item._id);
+            }
+        });
+
+        getCheckout({
+            variables: { products: productIds }
+        });
+
+        console.log(productIds)
+    }
+
     console.log(state);
 
     return (
@@ -59,7 +82,7 @@ const Cart = () => {
                         <strong>Total: ${calculateTotal()}</strong>
                         {
                             Auth.loggedIn() ?
-                                <button>
+                                <button onClick={submitCheckout}>
                                     Checkout
                                 </button>
                                 :
